dadccfadc71992fb768f9ab3524143a86514467b
Ahmed Gamal <ahmedgamal1452@gmail.com>
2022-08-18 04:34:16 +0200

    Apply (Slider and Tabs)

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
 import {
   set_Elments,
   set_Attribute,
-  show_OneItem,
+  matchAllScreens,
+  applyTap,
+  tabsFilter,
 } from "./modules/helper-functions.js";
 
 const courseTemplate = document.querySelector("#course-temp");
-const coursesList = document.querySelector(".courses-list");
-const form = document.getElementsByClassName("search-bar")[0];
-const mxql = window.matchMedia("(max-width: 800px)");
-let change = 0;
-let courses = [];
-let textserach = "";
+const matchFourCourses = window.matchMedia("(max-width: 1200px)");
+const matchThreeCourses = window.matchMedia("(max-width: 1080px)");
+const matchTwoCourses = window.matchMedia("(max-width: 850px)");
+const matchOneCourses = window.matchMedia("(max-width: 740px)");
+const tapsNodes = document.querySelectorAll("#pills-tab li");
+const tapsNodesArray = Array.from(tapsNodes);
+const mapCourses = new Map();
+let allCourses = [];
+let tabsCourses = [
+  "python",
+  "drawing",
+  "data science",
+  "aws",
+  "javascript",
+  "web development",
+  "excel",
+];
+const matchesScreen = [
+  matchFourCourses,
+  matchThreeCourses,
+  matchTwoCourses,
+  matchOneCourses,
+];
 
-form.addEventListener(
-  "submit",
-  (e) => {
-    e.preventDefault();
-    textserach = document
-      .getElementsByClassName("search-box")[0]
-      .value.toLowerCase();
-    courses.forEach((ele) => {
-      const isvisible = ele.title.toLowerCase().includes(textserach);
-      ele.course.classList.toggle("hide", !isvisible);
-    });
-  },
-  false
-);
-
-mxql.addEventListener("change", () => {
-  show_OneItem(change, courses, textserach);
-  change = !change;
+tabsCourses.forEach((tab) => {
+  tab = applyTap(tab);
+  mapCourses.set(tab, []);
 });
+
+tapsNodesArray.forEach((tap) => {
+  tap.addEventListener("click", function (e) {
+    let x = tap.querySelector("button").textContent;
+  });
+});
+
+matchesScreen.forEach((screen) => {
+  screen.addEventListener("change", () => {
+    matchAllScreens(mapCourses, tabsCourses, matchesScreen);
+  });
+});
+
 fetch("http://localhost:3000/courses")
   .then((res) => res.json())
   .then((json) => {
-    courses = json.map((data) => {
+    allCourses = json.map((data) => {
       const course = courseTemplate.content.cloneNode(true).children[0];
       for (let prop in data) {
         if (prop === "image") {
@@ -48,7 +65,18 @@ fetch("http://localhost:3000/courses")
           );
         } else set_Elments(course, "." + prop, data[prop]);
       }
-      coursesList.append(course);
+
       return { title: data["title"], course: course };
     });
+
+    allCourses.forEach((ele) => {
+      tabsCourses.forEach((tab) => {
+        let bol = tabsFilter(tab, ele.title.toLowerCase());
+        if (bol) {
+          tab = applyTap(tab);
+          mapCourses.get(tab).push(ele.course);
+        }
+      });
+    });
+    matchAllScreens(mapCourses, tabsCourses, matchesScreen);
   });
